65638364937c65568019e7482bb2fa49c0b72c75
clozada <crisx100preyo12@gmail.com>
2016-12-12 10:13:11 -0500

    Prueba

---
+++ Producto2clozadaUTC/conf/evolutions/default/2.sql
@@ -0,0 +1,19 @@
+# --- Sample dataset
+
+# --- !Ups
+
+
+
+
+
+insert into postre (id,name,descripcion,calorias) values (  1,'Pastel','pan','alta');
+
+
+
+
+
+
+# --- !Downs
+
+delete from postre;
+delete from company;
